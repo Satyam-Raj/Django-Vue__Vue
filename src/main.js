97b0b77e15d9287171a2a9a2ee388aaef7f022a0
Satyam Raj <44699922+Satyam-Raj@users.noreply.github.com>
2021-09-10 13:47:32 +0530

    product.vue

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-// window.axios = require('axios')
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
